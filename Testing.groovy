d854de937f1a07e51090cce92927dae1bd6edc01
Islands <drislands@pm.me>
2025-09-20 18:10:54 -0400

    Make Aspect and Principle If New
    Created two new outer class methods: makeAspectIfNew() and makePrincipleIfNew(). These are intended for only the core aspects, such as the 13 principles, and are for populating an empty database.
    
    The old way of having an explicit try/catch for every single one was too repetitive.

--- Testing.groovy
+++ Testing.groovy
@@ -90,89 +90,21 @@ class Testing {
     fillBasics()
   }
   static void fillBasics() {
-    try{
-      Aspect.getAspect('Forge')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Forge',"'Fire', I once read, 'is the winter that warms and the spring that consumes.' [The principle of the Forge transforms and destroys.]")
-      Principle.makePrinciple(a,'FF8E3F')
-    }
-    try{
-      Aspect.getAspect('Grail')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Grail',"Hunger, lust, the drowning waters. [The principle of the Grail honours both the birth and the feast.]")
-      Principle.makePrinciple(a,'FF614F')
-    }
-    try{
-      Aspect.getAspect('Knock')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Knock',"The principle called Knock permits no seal and no isolation. It thrusts us gleefully out of the safety of ignorance. [Knock opens doors and unseams barriers.]")
-      Principle.makePrinciple(a,'B54EFC')
-    }
-    try{
-      Aspect.getAspect('Sky')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Sky',"Wind, storm, echo, song; the intricacies of mathematics and the principles of flight. Law's touch is lighter than we sometimes think. [Matters of balance, harmony and necessity.]")
-      Principle.makePrinciple(a,'2C69E1')
-    }
-    try{
-      Aspect.getAspect('Edge')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Edge',"All conquest occurs at the Edge. One who dwells there is blind, and cannot be wounded. Another is strong, and grows stronger. [Edge is the principle of battle and of struggle.]")
-      Principle.makePrinciple(a,'D6DE4A')
-    }
-    try{
-      Aspect.getAspect('Scale')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Scale',"Hard without, hard within, hard to rouse, harder to subdue. [What is left of the crude powers of the deep earth.]")
-      Principle.makePrinciple(a,'CB9F4D')
-    }
-    try{
-      Aspect.getAspect('Rose')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Rose',"'The rose which encompasseth all'. Nine directions to new horizons. [Exploration? Enlightenment? Hope?]")
-      Principle.makePrinciple(a,'EF63FF')
-    }
-    try{
-      Aspect.getAspect('Heart')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Heart',"The Heart Relentless beats to protect the skin of the world we understand. [The Heart is the principle that continues and preserves.]")
-      Principle.makePrinciple(a,'FF7F8C')
-    }
-    try{
-      Aspect.getAspect('Lantern')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Lantern',"'Life is a pure flame, and we live by an invisible Sun within us.' - Thomas Browne. [Lantern is the principle of the secret place sometimes called the House of the Sun, and of the light above it.]")
-      Principle.makePrinciple(a,'FFE300')
-    }
-    try{
-      Aspect.getAspect('Moon')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Moon',"Secrets are soft; night is softer still; the sea speaks. It is not always wise to listen. [The nocturnal, the forgotten.]")
-      Principle.makePrinciple(a,'CBBCD6')
-    }
-    try{
-      Aspect.getAspect('Moth')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Moth',"I knew a man who captured moths in a bell-jar. On nights like this, he would release them one by one to die in the candle. [Moth is the wild and perilous principle of chaos and yearning.]")
-      Principle.makePrinciple(a,'F1E9C2')
-    }
-    try{
-      Aspect.getAspect('Nectar')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Nectar',"The green wealth in the world's veins; the pulse of the seasons. [Long ago, some called this principle Blood.]")
-      Principle.makePrinciple(a,'2CD391')
-    }
-    try{
-      Aspect.getAspect('Winter')
-    } catch(ignored) {
-      def a = Aspect.makeAspect('Winter',"... [Winter is the principle of silence, of endings, and of those things that are not quite dead.]")
-      Principle.makePrinciple(a,'BDEFFF')
-    }
-    try{
-      Aspect.getAspect('Memory')
-    } catch(ignored) {
-      Aspect.makeAspect('Memory',"Something remembered might be something understood. [Memories can be used to help with crafting and to upgrade Skills. Memories disappear each dawn - except Persistent ones.]")
-    }
+    makePrincipleIfNew('Forge',"'Fire', I once read, 'is the winter that warms and the spring that consumes.' [The principle of the Forge transforms and destroys.]",'FF8E3F')
+    makePrincipleIfNew('Grail',"Hunger, lust, the drowning waters. [The principle of the Grail honours both the birth and the feast.]",'FF614F')
+    makePrincipleIfNew('Knock',"The principle called Knock permits no seal and no isolation. It thrusts us gleefully out of the safety of ignorance. [Knock opens doors and unseams barriers.]",'B54EFC')
+    makePrincipleIfNew('Sky',"Wind, storm, echo, song; the intricacies of mathematics and the principles of flight. Law's touch is lighter than we sometimes think. [Matters of balance, harmony and necessity.]",'2C69E1')
+    makePrincipleIfNew('Edge',"All conquest occurs at the Edge. One who dwells there is blind, and cannot be wounded. Another is strong, and grows stronger. [Edge is the principle of battle and of struggle.]",'D6DE4A')
+    makePrincipleIfNew('Scale',"Hard without, hard within, hard to rouse, harder to subdue. [What is left of the crude powers of the deep earth.]",'CB9F4D')
+    makePrincipleIfNew('Rose',"'The rose which encompasseth all'. Nine directions to new horizons. [Exploration? Enlightenment? Hope?]",'EF63FF')
+    makePrincipleIfNew('Heart',"The Heart Relentless beats to protect the skin of the world we understand. [The Heart is the principle that continues and preserves.]",'FF7F8C')
+    makePrincipleIfNew('Lantern',"'Life is a pure flame, and we live by an invisible Sun within us.' - Thomas Browne. [Lantern is the principle of the secret place sometimes called the House of the Sun, and of the light above it.]",'FFE300')
+    makePrincipleIfNew('Moon',"Secrets are soft; night is softer still; the sea speaks. It is not always wise to listen. [The nocturnal, the forgotten.]",'CBBCD6')
+    makePrincipleIfNew('Moth',"I knew a man who captured moths in a bell-jar. On nights like this, he would release them one by one to die in the candle. [Moth is the wild and perilous principle of chaos and yearning.]",'F1E9C2')
+    makePrincipleIfNew('Nectar',"The green wealth in the world's veins; the pulse of the seasons. [Long ago, some called this principle Blood.]",'2CD391')
+    makePrincipleIfNew('Winter',"... [Winter is the principle of silence, of endings, and of those things that are not quite dead.]",'BDEFFF')
+    makeAspectIfNew('Memory',"Something remembered might be something understood. [Memories can be used to help with crafting and to upgrade Skills. Memories disappear each dawn - except Persistent ones.]")
+    makeAspectIfNew('Skill',"Anything can be learnt; everything can be forgotten.")
   }
   
   static String getInput(component=null) {
@@ -186,6 +118,22 @@ class Testing {
   }
   static List<String> splitPaste(String input) {
     input.split('[\r\n]+')*.trim().findAll { it }
+  }
+
+  static Aspect makeAspectIfNew(String name,String description) {
+    try {
+      return Aspect.getAspect(name)
+    } catch(ignored) {
+      return Aspect.makeAspect(name,description)
+    }
+  }
+  static Principle makePrincipleIfNew(String name,String description,String color) {
+    try {
+      return Principle.getPrinciple(name)
+    } catch(ignored) {
+      def a = makeAspectIfNew(name,description)
+      return Principle.makePrinciple(a,color)
+    }
   }
   
   @Canonical
